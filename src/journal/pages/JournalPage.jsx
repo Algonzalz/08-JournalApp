5101a0fe90807f759c655ddfcf26c236cc3569f7
henry-gonzalez <gonzalezhenry95@gmail.com>
2023-01-24 16:00:53 -0400

    add| clase 251

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,13 @@
-import { Typography } from "@mui/material"
+
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has</Typography  >
-
+      {/* <Typography>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has</Typography  > */}
+      {/* <NothingSelectedView/> */}
+      <NoteView/>
     </JournalLayout>
   )
 }
