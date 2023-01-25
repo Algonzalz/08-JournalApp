e94d3ceba00c095c17afc7c69f75519e443c483b
henry-gonzalez <gonzalezhenry95@gmail.com>
2023-01-25 14:49:50 -0400

    add | ultimos cambios antes del redux

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,6 @@
 
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 
@@ -8,6 +10,20 @@ export const JournalPage = () => {
       {/* <Typography>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has</Typography  > */}
       {/* <NothingSelectedView/> */}
       <NoteView/>
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
